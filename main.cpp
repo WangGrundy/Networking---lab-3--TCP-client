a94f24472cabc4416f6795754b57363175a33802
JDgoldvox <VoxcastJDgoldvox@gmail.com>
2024-02-11 14:57:06 -0500

    Finished

--- main.cpp
+++ main.cpp
@@ -5,22 +5,20 @@
 #include "socklib.h"
 
 std::string do_client(std::istream& in_stream);
+std::string build_string(std::istream& in_stream);
 int run_test();
 
 int main(int argc, char* argv[]) {
     SockLibInit(); //for windows
 
-
-
     /* TODO: ADD SETUP CODE HERE */
-
     return run_test();
 }
 
 std::string do_client(std::istream& in_stream) {
     /* TODO: ADD FUNCTION IMPLEMENTATION HERE */
 
-      //Create a socket, and connect to the server at IP ADDRESS 68.183.63.165 and PORT 7778.
+    //Create a socket, and connect to the server at IP ADDRESS 68.183.63.165 and PORT 7778.
     Socket socket(Socket::Family::INET, Socket::Type::STREAM);
     std::string str_address("68.183.63.165");
     Address address(str_address);
@@ -30,7 +28,7 @@ std::string do_client(std::istream& in_stream) {
     std::string msgSent;
 
     //Send a properly - formatted sort string to the server.
-    std::string msg((char*)&in_stream); //or "LIST 5 4 3 2 1"
+    std::string msg = build_string(in_stream);
     size_t nbytes_sent = socket.Send(msg.data(), msg.size());
 
     //Receive the server's response.
@@ -38,25 +36,63 @@ std::string do_client(std::istream& in_stream) {
     size_t nbytes_recved = socket.Recv(buffer, sizeof(buffer));
 
     //Create a std::string from the server's response and return it.
-    std::string msgRecieed(buffer, nbytes_recved);
+    std::string msgRecieved(buffer, nbytes_recved);
 
-    std::cout << "HELLO\n;";
-
-    return msgRecieed;
+    return msgRecieved;
 }
 
 std::string build_string(std::istream& in_stream) {
     /* TODO (optional): implement this function */
+    using namespace std;
 
-      //Get a line from the given istream.
-      //std::string line = in_stream.getline(in_stream);
+    //Get a line from the given istream.
+    std::vector<string> numbersAsString;
+    string line;
 
-      //Take a line of input from the given istream and parse it into an integer(if it is an integer) or a float(if it is a float).
-    int lineInt;
-    float lineFloat;
+    //Take a line of input from the given istream and parse it into an integer(if it is an integer) or a float(if it is a float).
+    while (getline(in_stream, line)) {
+        //condition to put only ints or floats inside our number vector
+        int currentIndex = 0;
+        string completeString;
+        //read all chars
+        while (currentIndex != line.length()) {
+            int currentItem = (int)line[currentIndex];
 
-    //Add the parsed number to a string, which will ultimately be sent to the server.
-    std::string msg;
+            //check if number
+            if (currentItem >= 48 && currentItem <= 57) {
+
+            }
+            //if its a decimal
+            else if (currentItem == 46) {
+
+            }
+            //if its a dash
+            else if (currentItem == 45) {
+
+            }
+            else {
+                completeString.clear();
+                break;
+            }
+
+            completeString += line[currentIndex];
+            currentIndex++;
+        }
+        
+        if (!completeString.length() == 0) {
+            numbersAsString.push_back(completeString);
+        }
+        
+    }
+
+    string correctlyFormattedLine;
+    correctlyFormattedLine += "LIST";
+    for (string str : numbersAsString) {
+        correctlyFormattedLine += " ";
+        correctlyFormattedLine += str;
+    }
+
+    cout << correctlyFormattedLine << endl;
 
     //When the user enters the string done, the string is complete and should be returned.
     std::string ans = "";
@@ -64,7 +100,7 @@ std::string build_string(std::istream& in_stream) {
         std::cin >> ans;
     }
 
-    return msg;
+    return correctlyFormattedLine;
 }
 
 int run_test() {
